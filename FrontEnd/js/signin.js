7a734638e4a77c7eedbc71b53626cd4fb0f00023
BrdML <bert.m310@gmail.com>
2023-08-23 00:50:22 +0200

    ajout page page de connexion

---
+++ FrontEnd/js/signin.js
@@ -0,0 +1,53 @@
+const apiUrl = 'http://localhost:5678/api/users/';
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+const loginForm = document.getElementById('login-form');
+const successMessage = document.getElementById('success-message');
+const errorMessage = document.getElementById('error-message');
+
+
+const logIn = async () => {
+
+    loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const email = emailInput.value;
+        const password = passwordInput.value;
+
+        try {
+            const response = await fetch(apiUrl + "login", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': "application/json; charset=UTF-8",
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password
+                })
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                // console.log(data);
+
+                localStorage.setItem('userId', data.userId);
+                localStorage.setItem('token', data.token);
+
+                successMessage.textContent = 'Connexion réussi';
+                window.location.replace("../index.html");
+            } else {
+                errorMessage.textContent = 'Identifiants invalides. Veuillez réessayer.';
+            }
+
+        } catch (error) {
+            console.error('Une erreur s\'est produite :', error);
+            errorMessage.textContent = 'Une erreur s\'est produite. Veuillez réessayer plus tard.';
+        }
+        
+    });
+}
+
+const init = async () => {
+    logIn();
+}
+init();
